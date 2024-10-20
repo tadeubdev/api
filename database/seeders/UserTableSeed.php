47e90aa8b545007d0dca651db8d0b3073594219d
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2024-10-20 20:47:07 -0300

    feat: add use table seed to users table

---
+++ database/seeders/UserTableSeed.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
