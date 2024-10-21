1c829fad71b7bd436812fc4f55f23e56d1340623
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2024-10-20 21:35:24 -0300

    feat: add vue-toastification for displaying toast messages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
 import App from './App.vue'
 import { createApp } from 'vue'
 import router from './router/route'
+import Toast from "vue-toastification"
 
 import './App.css'
+import "vue-toastification/dist/index.css"
 
 const app = createApp(App)
 app.use(router)
+app.use(Toast, {});
 app.mount('#app')
