0767291619a5ea34ee0d8644a8b21fb9f5320904
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2024-10-20 21:46:05 -0300

    refactor: update Home.vue to add logout functionality

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,8 +1,30 @@
 <template>
-    <div>
-        Home
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Welcome Our App!</h2>
+            <p class="text-center text-lg text-gray-500">
+                You're logged in!
+            </p>
+
+            <p class="mt-6">
+                <button
+                    @click="handleLogout"
+                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                    Log out
+                </button>
+            </p>
+        </div>
     </div>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const handleLogout = () => {
+    localStorage.removeItem('token');
+    router.push('/login');
+};
 </script>
