bf0ff079ac494e83df9bb7cc52acbeb9f443ea74
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2024-10-20 21:35:43 -0300

    refactor: update token check in router.beforeEach()

--- resources/js/router/route.js
+++ resources/js/router/route.js
@@ -27,7 +27,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
     const publicPages = ['/login', '/register'];
     const authRequired = !publicPages.includes(to.path);
-    const loggedIn = localStorage.getItem('user');
+    const loggedIn = localStorage.getItem('token');
 
     if (authRequired && !loggedIn) {
         return next('/login');
