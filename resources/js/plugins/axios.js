872b452b096b086ad156635392c0694308c6f1c9
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2024-10-20 21:25:16 -0300

    chore: add axios support and config file

---
+++ resources/js/plugins/axios.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+// const api = axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+const api = axios.create({
+    baseURL: import.meta.env.VITE_APP_URL,
+    headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+    }
+});
+
+// if token exists, set the default header
+const token = localStorage.getItem('token');
+if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
+
+export default api;
